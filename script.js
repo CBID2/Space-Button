f3ca6e3c12cf014bc73eadf9c8844bf62a145f26
Christine Belzie <105683440+CBID2@users.noreply.github.com>
2023-02-27 21:13:56 -0500

    Added files from CodePen

---
+++ script.js
@@ -0,0 +1,4 @@
+function myFunction() {
+     document.body.style.backgroundImage = "url('https://cdn.pixabay.com/photo/2017/08/15/08/23/stars-2643089__480.jpg')";  document.getElementById("spaceEntry").textContent = "Welcome to Space!"; document.getElementById("spaceEntry").classList.add("spacey-font");
+
+  }
